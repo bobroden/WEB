b3a517a2c4d12c8f71d74a6d50e557724af42ab3
bobro <bobrow.bobro2015@yandex.ru>
2020-11-11 01:04:37 +0300

    feat: add main functions for game

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let labMatrix = [];
 
 document.querySelector('.enter').addEventListener('click', () => {
 	document.querySelector('.aut').classList.add('hidden');
@@ -5,13 +6,31 @@ document.querySelector('.enter').addEventListener('click', () => {
 });
 
 function check() {
-	main_Width = document.body.clientWidth;
 	let num1 = +document.querySelector('.X').value;
     let num2 = +document.querySelector('.Y').value;
     if((num1 <= 30 && num1 >= 5 && num1 % 1 === 0) && (num2 <= 30 && num2 >= 5 && num2 % 1 === 0)) {
         document.querySelector('.help').classList.add('vissible');
         document.querySelector('.canvas-game').setAttribute('width', 30 * num1);
         document.querySelector('.canvas-game').setAttribute('height', 30 * num2);
+        for(let i = 0; i < num1; i++) {
+    		labMatrix[i] = [];
+    		for(let j = 0; j < num2; j++) {
+        		labMatrix[i][j] = 0;
+    		}
+		}
+		let canvas = document.querySelector('.canvas-game');
+		let context = canvas.getContext("2d");
+		for (let x = 0.5; x < 30 * num1; x += 30) {
+			context.moveTo(x, 0);
+			context.lineTo(x, 30 * num2);
+		}
+		for (var y = 0.5; y < 30 * num2; y += 30) {
+			context.moveTo(0, y);
+			context.lineTo(30 * num1, y);
+		}
+		context.setLineDash([2, 3]);
+		context.strokeStyle = "#C4C4C4";
+		context.stroke();
     }
     else {
         document.querySelector('.help').classList.remove('vissible');
@@ -21,3 +40,79 @@ function check() {
 document.querySelector('.X').addEventListener('input', check);
 
 document.querySelector('.Y').addEventListener('input', check);
+
+function Player(x, y) {
+  this.x = x;
+  this.y = y;
+
+  this.Up = function() {
+  	this.y--;
+  }
+  this.Right = function() {
+  	this.x++;
+  }
+  this.Down = function() {
+  	this.y++;
+  }
+  this.Left = function() {
+  	this.x--;
+  }
+}
+
+function Cell(top, right, bottom, left) {
+	this.top = top;
+  	this.right = right;
+  	this.bottom = bottom;
+  	this.left = left;
+}
+
+function drawCell(Cell, Player) {
+	let canvas = document.querySelector('.canvas-game');
+	let context = canvas.getContext("2d");
+	if(Cell.top) {
+		context.moveTo(Player.x, Player.y);
+		context.lineTo(Player.x + 30, Player.y);
+	}
+	if(Cell.right) {
+		context.moveTo(Player.x + 30, Player.y);
+		context.lineTo(Player.x + 30, Player.y + 30);
+	}
+	if(Cell.bottom) {
+		context.moveTo(Player.x, Player.y + 30);
+		context.lineTo(Player.x + 30, Player.y + 30);
+	}
+	if(Cell.left) {
+		context.moveTo(Player.x, Player.y);
+		context.lineTo(Player.x, Player.y + 30);
+	}
+	context.strokeStyle = "#000";
+	context.stroke();
+	//labMatrix[Player.x][Player.y] = Cell;
+}
+
+function drawPlayer(Player) {
+	let canvas = document.querySelector('.canvas-game');
+	let context = canvas.getContext("2d");
+	context.beginPath();
+	context.arc(Player.x + 15, Player.y + 15, 10, 0, 2*Math.PI, false);
+	context.fillStyle = 'blue';
+	context.fill();
+	context.lineWidth = 1;
+	context.strokeStyle = 'blue';
+	context.stroke();
+}
+
+function clearPlayer(Player) {
+	let canvas = document.querySelector('.canvas-game');
+	let context = canvas.getContext("2d");
+	context.clearRect(Player.x + 1, Player.y + 1, 29, 29);
+}
+
+document.querySelector('.Up').addEventListener('click', () => {
+	clearPlayer(palyer1);
+})
+
+let palyer1 = new Player(0, 0);
+let cell1 = new Cell(true, true, true, false);
+drawCell(cell1, palyer1);
+drawPlayer(palyer1);
